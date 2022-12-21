17cb980672da36c5e3bd501b6b35f390dcf000c1
VictoriaBladow <vbladow9999@yahoo.com>
2022-12-20 18:38:35 -0600

    commit tree 89d4c0504e12e4da09f682158d661cfd6bf1eea7

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun('bathe my dog')
+
+function mondayWork(job = 'go to the office'){
+    return `This Monday, I will ${job}.`
+}
+
+mondayWork('work from home')
+
+function wrapAdjective(exam = "*"){
+    return function ex(guess = 'special'){
+        return `You are ${exam}${guess}${exam}!`}
+    
+}
